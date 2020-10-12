5208497a408d90132c44e641ab32db16d1b86127
Schöpf <csas8782@uibk.ac.at>
2020-10-12 18:21:01 +0200

    rewrite API data download functionality

--- fetchCbioData.r
+++ fetchCbioData.r
@@ -1,34 +1,68 @@
-library(ggplot2)
-library(gridExtra)
-library(tibble)
+library(tidyverse)
 library(cBioPortalData)
 library(AnVIL)
 
-#pull data from API and get gene expression information
+################
+# input vars  #
+################
+selStudyList = c("gbm_tcga_pan_can_atlas_2018", "chol_nccs_2013") # a vector of studyIds to investigate (pull e.g. from manually imported data)
+selGenes = c("MYC", "BASP1", "PHB") # a vector of hugoGeneSymbols
+
 cbio <- cBioPortal()
 
-all.studies <- getStudies(cbio)
-filtered.studies <- all.studies %>% filter(grepl(".*myelo.*|.*lympho.*", name, ignore.case = TRUE))
-filtered.studies <- filtered.studies[- grepl("cell line", filtered.studies$name, ignore.case = TRUE), ]
-
-sampleList <- list()
-expList <- list()
-i <- 1
-for(studyId in filtered.studies$studyId) {
-  sampleList[[i]] <- allSamples(cbio, studyId)
-  expList[[i]] <- molecularData(api = cbio,
-                molecularProfileIds = c("acc_tcga_rna_seq_v2_mrna"),
-                entrezGeneIds = c(4609, 10409),
-                sampleIds = sampleList[[i]]$sampleId)
-  i <- i + 1
+# translates hugoGeneSymbols to entrezGeneIds
+# this function takes LONG, therefore cache when possible
+translateGeneSymbols <- function(hugoGeneSymbols) {
+  geneTablePage <- 1
+  translatorTable <- tibble()
+  while (dim(translatorTable)[[1]] != length(selGenes)) {
+    geneTable <- geneTable(cbio, pageNumber = geneTablePage)
+    filteredGeneTable <-
+      geneTable[geneTable$hugoGeneSymbol %in% selGenes, ]
+    translatorTable <- rbind(translatorTable, filteredGeneTable)
+    geneTablePage <- geneTablePage + 1
+    if (geneTablePage > 170) {
+      stop("Invalid gene symbols")
+    }
+  }
+  #cache for faster reloading, TODO: check for updates in input data
+  write.csv(translatorTable, file = "translatorTable.csv", row.names = FALSE)
+  return(translatorTable)
 }
 
-concatSampleList <- do.call(rbind, sampleList)
-sampleIdList <- paste0(samples$patientId, '-01')
-testList <- c("TCGA-OR-A5J1-01", "TCGA-OR-A5J2-01")
+getMolecularData <- function(study) {
+  # make sampleId list
+  samples <- allSamples(cbio, studyId = study)
+  
+  # make molecularProfileId (mRNA)
+  molecularProfile <- paste0(study, "_rna_seq_v2_mrna")
+  
+  # fetch data
+  mols <- molecularData(
+    api = cbio,
+    molecularProfileIds = molecularProfile,
+    entrezGeneIds = translatorTable$entrezGeneId,
+    sampleIds = samples$sampleId
+  )
+  return(mols)
+}
 
-expressionData <- molecularData(api = cbio,
-                      molecularProfileIds = c("acc_tcga_rna_seq_v2_mrna"),
-                      entrezGeneIds = c(4609, 10409),
-                      sampleIds = sampleIdList
-)
+#################
+# main function #
+#################
+
+#TODO try-catch
+translatorTable <- read.csv("translatorTable.csv")
+if(!exists("translatorTable")) {
+  translatorTable <- translateGeneSymbols(selGenes)
+}
+
+# get data from all selected studies
+all.data <- tibble()
+for (study in selStudyList) {
+  new.data <- getMolecularData(study)
+  all.data <- rbind(all.data, new.data[[1]])
+}
+
+# restructure output table
+
