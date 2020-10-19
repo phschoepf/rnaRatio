46fa3e24fb28e0e980ee750d5a871542da30f188
Schöpf <csas8782@uibk.ac.at>
2020-10-19 18:14:06 +0200

    unify plotting functions, work on combined large plot

--- plotAll.r
+++ plotAll.r
@@ -1,3 +1,4 @@
+library(rlang)
 library(ggplot2)
 library(cowplot)
 library(grid)
@@ -7,10 +8,31 @@ library(tidyverse)
 library(tibble)
 library(ggpmisc)
 
-source("parseInputData.r")
+#source("parseInputData.r")
+source("plotMethods.r")
 
-source("plotMycBaspRatios.r")
-source("plotMycPhbRatios.r")
-source("plotMycAbsolute.r")
-source("plotBaspAbsolute.r")
+mycbasp_cell <- plotCells(cellLines, "MYC", "BASP1", c(-2,4))
+mycbasp_pat <- plotPatients(allPatientData, "MYC", "BASP1", c(-2,4))
+#exportToPng(baseName = "myc_basp1_ratios", mycbasp_cell, mycbasp_pat)
+
+mycphb_cell <- plotCells(cellLines, "MYC", "PHB", c(-3,2))
+mycphb_pat <- plotPatients(allPatientData, "MYC", "PHB", c(-3,2))
+#exportToPng(baseName = "myc_phb_ratios", mycphb_cell, mycphb_pat)
+
+myc_cell <- plotCells(cellLines, "MYC", lims = c(NA,400))
+myc_pat <- plotPatients(allPatientData, "MYC", lims =  c(NA,15000))
+#exportToPng(baseName = "myc_absolute", myc_cell, myc_pat)
+
+basp1_cell <- plotCells(cellLines, "BASP1", lims = c(NA,700))
+basp1_pat <- plotPatients(allPatientData, "BASP1", lims =  c(NA,15000))
+#exportToPng(baseName = "basp1_absolute", basp1_cell, basp1_pat)
+
+#combined large plot
+tiff("allPlots.tiff", width = 170, height = 200, units = "mm", res = 300)
+grid.arrange(myc_cell, myc_pat,
+             basp1_cell, basp1_pat,
+             mycbasp_cell, mycbasp_pat,
+             layout_matrix = cbind(c(1,3,5), c(2,4,6))
+)
+dev.off()
 #source("RPKMvsRSEMdemo.r") does not work currently w/ new APIrequest input system
--- plotMethods.r
+++ plotMethods.r
@@ -22,47 +22,112 @@ exportToPng <- function(baseName, cellplot, patplot) {
   dev.off()
 }
 
-plotPatients <- function(data, is_ratio, xlab) {
+
+plotPatients <- function(in_data, gene1, gene2 = NULL, lims) {
+  if(is.null(gene2)) {
+    # only 1 gene, plot absolutes
+    plotTitle <- paste0(gene1, " RNA expression")
+    axisTitle <- "RNA expression, RNAseq v2 RSEM (Batch normalized from Illumina HiSeq_RNASeqV2)"
+    plotBy <- expr(!!sym(gene1))
+    
+  } 
+  else {
+    # 2 genes, plot gene1 to gene2 ratio on log scale
+    plotTitle <- paste0(gene1, " / ", gene2, " RNA expression ratio")
+    axisTitle <- expression(paste(log[10], " RNA expression ratio, RNAseq v2 RSEM (Batch normalized from Illumina HiSeq_RNASeqV2)"))
+    plotBy <- expr(log10(!!sym(gene1) / !!sym(gene2)))
+  }
+  
+  plotdata <- in_data %>% 
+    group_by(Origin) %>% 
+    arrange(desc(!!plotBy, by_group = T)) %>%
+    mutate(toPlot = !!plotBy)
+  
   patplot <-
-    ggplot(data, aes(
-      y = reorder(Origin, ~data[ncol(data)], FUN = median), # plot the last col (possibly TODO)
-      x = ~log10(data[ncol(data)]),
+    ggplot(plotdata, aes(
+      x = Origin,
+      y = !!plotBy,
       na.rm = TRUE
     )) +
-    coord_cartesian(xlim = (c(-3,2)), expand = F) + 
-    theme_grey(base_size = 20) +
-    labs(title = "Patient samples", y = "" , x = xlab) +
-    stat_boxplot(geom = "errorbar", width = 0.6, lwd = 1) +
-    geom_boxplot(outlier.shape = NA,
-                 lwd = 1,
-                 fatten = 1.2) +
+    coord_cartesian(ylim = lims) +
+    theme_grey(base_size = 5) +
+    labs(
+      title = plotTitle,
+      x = "",
+      y = axisTitle
+    ) +
     geom_jitter(
       shape = 16,
-      position = position_jitter(w = 0, h = 0.15),
-      size = 0.8,
+      position = position_jitter(w = 0.25, h = 0),
+      size = 0.6,
       color = "#f39200"
-    )
+    ) +
+    stat_boxplot(geom = "errorbar", width = 0.6) +
+    geom_boxplot(alpha = 0.8,
+                 outlier.shape = NA,
+                 fatten = 1.2
+    ) 
+    
 }
 
-plotCells <- function(data, is_ratio, xlab) {
+
+plotCells <- function(in_data, gene1, gene2 = NULL, lims) {
+  if(is.null(gene2)) {
+    # only 1 gene, plot absolutes
+    plotTitle <- paste0(gene1, " RNA expression")
+    axisTitle <- "RNA expression, RNAseq v2 RSEM (Batch normalized from Illumina HiSeq_RNASeqV2)"
+    plotBy <- expr(!!sym(gene1))
+    
+  } 
+  else {
+    # 2 genes, plot gene1 to gene2 ratio on log scale
+    plotTitle <- paste0(gene1, " / ", gene2, " RNA expression ratio")
+    axisTitle <- expression(paste(log[10], " RNA expression ratio, RNAseq v2 RSEM (Batch normalized from Illumina HiSeq_RNASeqV2)"))
+    plotBy <- expr(log10(!!sym(gene1) / !!sym(gene2)))
+  }
+  
+  plotdata <- in_data %>% 
+    group_by(Origin) %>% 
+    arrange(desc(!!plotBy, by_group = T)) %>%
+    mutate(toPlot = !!plotBy)
+  
   cellplot <-
-    ggplot(cellLines, aes(
-      y = reorder(Origin, data[ncol(data)], FUN = median), # plot the last col (possibly TODO)
-      x = log10(!!data[ncol(data)]),
+    ggplot(plotdata, aes(
+      x = Origin,
+      y = !!plotBy,
       na.rm = TRUE
     )) +
-    coord_cartesian(xlim = (c(-3,2)), expand = F) +
-    theme_grey(base_size = 20) +
-    labs(title = "Cell lines", y = "", x = xlab) +
-    stat_boxplot(geom = "errorbar", width = 0.6, lwd = 1) +
-    geom_boxplot(outlier.shape = NA,
-                 lwd = 1,
-                 fatten = 1.2) +
+    coord_cartesian(ylim = lims) +
+    theme_grey(base_size = 5) +
+    labs(
+      title = plotTitle,
+      x = "",
+      y = axisTitle
+    ) +
     geom_jitter(
       shape = 16,
-      position = position_jitter(w = 0, h = 0.15),
-      size = 0.9,
-      color = "#f39200") +
-    geom_point(data = selectedCellLines, color = "red", size = 4) +
-    geom_text(data = selectedCellLines, aes(label = Name), nudge_x = 0.02, nudge_y = 0.65)
+      position = position_jitter(w = 0.25, h = 0),
+      size = 0.6,
+      color = "#f39200"
+    ) +
+    stat_boxplot(geom = "errorbar", width = 0.6) +
+    geom_boxplot(alpha = 0.8,
+                 outlier.shape = NA,
+                 fatten = 1.2) +
+    geom_point(data = selectedCellLines, color = "red", size = 2) +
+    geom_text(
+      data = selectedCellLines,
+      aes(label = Name),
+      nudge_x = 0.5,
+      nudge_y = 0
+    ) + 
+    annotate(
+      geom = "table",
+      x = Inf,
+      y = Inf,
+      label = list(
+        selectedCellLines %>% mutate(rounded = round(!!plotBy, 2)) %>% select(Name, rounded) 
+      ),
+      size = 0.7
+    )
 }
